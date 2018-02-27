2a19eeb68f6c70488fc95b42ca499b8713c1ebfb
Nick Adams <nick.adams@icloud.com>
2018-02-26 18:12:01 -0800

    improved testing, ready for PR

--- database/seed-database.test.js
+++ database/seed-database.test.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 const {Business} = require('./db-helpers');
 
-beforeAll(() => {
+beforeEach(() => {
   mongoose.connect('mongodb://localhost/businessInfo');
 });
 
-describe('should check for seeded data to database', () => {
-  test('should add seeded data to database', done => {
+describe('should have seeded data in database', () => {
+  test('should have data in database', done => {
     Business.find({id: 201}).exec( (err, result) => {
       if (err) { console.log(err); }
+      mongoose.connection.close();
       expect(result).toBeDefined();
       done();
     });
   }); 
-});
-
-afterAll(() => {
-  mongoose.disconnect();
+  test('should have 200 records in database', done => {
+    Business.find({}).exec((err, result) => {
+      if (err) { console.log(err); }
+      mongoose.connection.close();
+      expect(result.length).toBe(200);
+      done();
+    });
+  });
 });
