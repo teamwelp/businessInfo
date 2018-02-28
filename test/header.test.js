f4640d004b9cea0b5f457c3745ba3c17a79c1b51
Nick Adams <nick.adams@icloud.com>
2018-02-28 10:36:36 -0800

    wrote tests for rendering header

--- test/header.test.js
+++ test/header.test.js
@@ -4,9 +4,23 @@ import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../client/dev/index';
 
-Enzyme.configure({ adapter: new Adapter()} );
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders a component', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('h1')).toHaveLength(1);
+describe('Header Component', () => {
+  test('renders a component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1')).toHaveLength(1);
+  });
+  test('renders restaurant name from starter data', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toBe('Oleg\'s Burger Palace');
+  });
+  test('renders restaurant claimedByOwner from starter data', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h3').text()).toBe('Restaurant Claimed By Owner: true');
+  });
+  test('renders restaurant metatags', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h2').text()).toBe('Soul Food, Bars, Dinner, Brunch');
+  });
 });
