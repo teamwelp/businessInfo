c2d6c6080592c94a36aae840c96be640198a8d2e
Nick Adams <nick.adams@icloud.com>
2018-03-02 13:39:57 -0800

    added html and css for rest of header data

--- client/dev/header.jsx
+++ client/dev/header.jsx
@@ -1,5 +1,9 @@
 import FaQuestion from 'react-icons/lib/fa/question-circle';
 import FaCheck from 'react-icons/lib/fa/check-circle';
+import FaLocation from 'react-icons/lib/fa/map-marker';
+import FaLink from 'react-icons/lib/fa/external-link';
+import FaPhone from 'react-icons/lib/fa/phone';
+import FaCellPhone from 'react-icons/lib/fa/mobile';
 import React from 'react';
 import styles from './header.css';
 
@@ -17,16 +21,45 @@ export default function (props) {
     priceScale = priceScale.concat('$');
   }
   return (
-    <div className={styles.header}>
-      <div className={styles.name}>{props.data.name}</div>
-      <div className={styles.claimdiv}>
-        <div className={styles.claimicon}>{claimIcon}</div>
-        <div className={claimStyle}>{claimedContent}</div>
+    <div>
+      <div className={styles.header}>
+        <div className={styles.name}>{props.data.name}</div>
+        <div className={styles.claimdiv}>
+          <div className={styles.claimicon}>{claimIcon}</div>
+          <div className={claimStyle}>{claimedContent}</div>
+        </div>
+        <div className={styles.bottomHeader}>
+          {priceScale}
+          <div className={styles.bullet}>&bull;</div>
+          <div className={styles.metatags}>{props.data.metatags.join(', ')}</div>
+        </div>
       </div>
-      <div className={styles.bottomHeader}>
-        {priceScale}
-        <div className={styles.bullet}>&bull;</div>
-        <div className={styles.metatags}>{props.data.metatags.join(', ')}</div>
+      <div className={styles.contact}>
+        <div className={styles.address}>
+          <div className={styles.grayicon}><FaLocation /></div>
+          <div className={styles.addressBox}>
+            {props.data.addressNumber} {props.data.addressStreet} 
+            <br />
+            {props.data.addressCity}, {props.data.addressState} {props.data.addressZip}
+          </div>
+        </div>
+        <div>
+          <div className={styles.grayicon}><FaPhone /></div>
+          <div className={styles.addressBox}>
+            ({props.data.phoneAreaCode}) {props.data.phoneOfficeCode}-{props.data.phoneLineCode}
+          </div>
+        </div>
+        <div>
+          <div className={styles.grayicon}><FaLink /></div>
+          <a className={styles.contactLink} href={props.data.businessLink}>
+            {props.data.businessLink}
+          </a>
+        </div>
+        <div>
+          <div className={styles.grayicon}><FaCellPhone /></div>
+          <a className={styles.contactLink} href="#">Send to your Phone</a>
+        </div>
+        <div className={styles.clearBoth} />
       </div>
     </div>
   );
